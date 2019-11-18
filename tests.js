41568143cdbd77f0f234f1d012e0e3c14b760994
Marc Tannous <tannous.marc@gmail.com>
2019-11-18 10:55:03 +0000

    Tests init

--- tests.js
+++ tests.js
@@ -4,3 +4,15 @@ import { If, isZero } from './conditionals.js';
 import { Zero, Add, Sub, Succ } from './churchNumerals.js';
 import { lessThanOrEqual, isEqual, greaterThan } from './equality.js';
 import { displayNumber } from './utils.js';
+
+const assert = (expr, message) => {
+    if(expr){
+        console.log('\x1b[32m%s\x1b[0m', message); 
+    }
+    else{
+        console.log('\x1b[31m%s\x1b[0m', message); 
+    }
+}
+
+assert(I(True) === True, "I(True) evaluates to True");
+assert(I(False) === False, "I(False) evaluates to False");
